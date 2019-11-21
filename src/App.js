d013e980fc6becd153d5d095ae41b5b1e127423f
Baptiste Doucerain <doucerain.b@gmail.com>
2019-11-21 10:47:06 +0100

    Put back search list

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import React from "react";
-import "./App.css";
-import SearchList from "./SearchList";
-import { DndProvider } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
-import Slider from "./Slider";
-// import whyDidYouRender from "@welldone-software/why-did-you-render";
-
-// whyDidYouRender(React, {
-//   onlyLogs: true,
-//   titleColor: "green",
-//   diffNameColor: "darkturquoise"
-// });
+import React from 'react'
+import './App.css'
+import SearchList from './SearchList'
+import { DndProvider } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+import Slider from './Slider'
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
-      {/* <div className="App" style={{ display: "flex", flexDirection: "row" }}>
+      <div className="App" style={{ display: 'flex', flexDirection: 'row' }}>
         <SearchList></SearchList>
         <Slider></Slider>
-      </div> */}
-      <Slider></Slider>
+      </div>
     </DndProvider>
-  );
+  )
 }
 
-// App.whyDidYouRender = true;
-
-export default App;
+export default App
