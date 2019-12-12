ca778bd0d07770d01a9f05587793f6e44ab53f17
Baptiste Doucerain <doucerain.b@gmail.com>
2019-12-12 15:41:41 +0100

    Supprimer la carte ajoutée

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,79 +1,116 @@
-import React, { useState, useCallback } from 'react'
-import SortableCard from './SortableCard'
-import { useDrop } from 'react-dnd'
+import React, { useState, useCallback, useEffect, useRef } from "react";
+import SortableCard from "./SortableCard";
+import { useDrop } from "react-dnd";
 
 const ITEMS = [
-  { id: '10', label: 'Bonjour' },
-  { id: '11', label: 'Hello' },
-  { id: '12', label: 'Hi' },
-  { id: '13', label: 'Sayonara' }
-]
+  { id: "10", label: "Bonjour" },
+  { id: "11", label: "Hello" },
+  { id: "12", label: "Hi" },
+  { id: "13", label: "Sayonara" }
+];
 
 const Slider = () => {
-  const [cards, setcardList] = useState(ITEMS)
+  const [cards, setcardList] = useState(ITEMS);
+
+  // console.log("Cards : ", cards);
 
   const findCard = useCallback(
     id => {
-      const card = cards.filter(c => `${c.id}` === id)[0]
+      const card = cards.filter(c => `${c.id}` === id)[0];
       return card
         ? {
             card,
             index: cards.indexOf(card)
           }
-        : null
+        : null;
     },
     [cards]
-  )
+  );
 
   const moveCard = useCallback(
     (id, atIndex) => {
-      const cardFound = findCard(id)
+      const cardFound = findCard(id);
 
       if (cardFound) {
-        const { card, index } = cardFound
-        cards.splice(index, 1)
-        cards.splice(atIndex, 0, card)
+        const { card, index } = cardFound;
+        cards.splice(index, 1);
+        cards.splice(atIndex, 0, card);
 
-        setcardList([...cards])
+        setcardList([...cards]);
       }
     },
     [cards, findCard]
-  )
+  );
 
   const addCard = useCallback(
     (atIndex, card) => {
       // if (!cards.find(card => card.id === item.id)) {
-      cards.splice(atIndex, 0, card)
-      setcardList([...cards])
+      cards.splice(atIndex, 0, card);
+      setcardList([...cards]);
       // }
     },
     [cards]
-  )
+  );
+
+  const addFakeCard = useCallback(
+    (atIndex, card) => {
+      cards.splice(atIndex, 0, card);
+      setcardList([...cards]);
+    },
+    [cards]
+  );
+
+  const cleanFakeCard = useCallback(() => {
+    const resultTab = cards.filter(card => {
+      return !card.isFake;
+    });
+    setcardList([...resultTab]);
+  }, [cards]);
 
   const deleteCard = useCallback(
     atIndex => {
-      cards.splice(atIndex, 1)
+      cards.splice(atIndex, 1);
     },
     [cards]
-  )
+  );
 
   const cardAlreadyExists = useCallback(
     id => {
       if (cards.find(card => card.id === id)) {
-        return true
+        return true;
       }
 
-      return false
+      return false;
     },
     [cards]
-  )
+  );
 
-  const [, drop] = useDrop({ accept: 'card' })
+  const [{ isOverSlider }, drop] = useDrop({
+    accept: "card",
+    collect: monitor => ({
+      isOverSlider: monitor.isOver()
+    })
+  });
 
-  console.log(cards)
+  const prevIsover = useRef(false);
+
+  useEffect(() => {
+    if (prevIsover.current === true && isOverSlider === false) {
+      console.log("Supprimer la carda joutée");
+    }
+
+    prevIsover.current = isOverSlider;
+  }, [isOverSlider]);
+
+  console.log("isOverSlider :", isOverSlider);
+
+  console.log(cards);
 
   return (
-    <div ref={drop} style={{ border: '2px solid black', margin: '40px', width: '500px' }}>
+    <div
+      ref={drop}
+      style={{ border: "2px solid black", margin: "40px", width: "500px" }}
+    >
       {cards.map((card, index) => {
         return (
           <SortableCard
@@ -86,11 +123,14 @@ const Slider = () => {
             cardAlreadyExists={cardAlreadyExists}
             findCard={findCard}
             deleteCard={deleteCard}
+            cleanFakeCard={cleanFakeCard}
+            addFakeCard={addFakeCard}
+            isFake={card.isFake}
           ></SortableCard>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
--- src/SortableCard.jsx
+++ src/SortableCard.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import { useDrop, useDrag } from 'react-dnd'
+import React from "react";
+import { useDrop, useDrag } from "react-dnd";
 
-const SortableCard = ({ id, label, moveCard, addCard, findCard, deleteCard }) => {
-  const originalIndex = findCard(id).index
+const SortableCard = ({
+  id,
+  label,
+  moveCard,
+  addCard,
+  findCard,
+  deleteCard,
+  cleanFakeCard,
+  addFakeCard,
+  isFake
+}) => {
+  const originalIndex = findCard(id).index;
 
   const [{ isDragging }, drag] = useDrag({
-    item: { type: 'card', id, originalIndex, cardFromSearch: false },
+    item: { type: "card", id, originalIndex, cardFromSearch: false, isFake },
     collect: monitor => {
-      const isItemBeingDragged = monitor.getItem() && monitor.getItem().id === id
+      const isItemBeingDragged =
+        monitor.getItem() && monitor.getItem().id === id;
       const isStillBeingDragged = !!(
         monitor.getItem() && monitor.getItem().isStillBeingDragged
-      )
+      );
 
       return isStillBeingDragged
         ? {
@@ -18,39 +29,40 @@ const SortableCard = ({ id, label, moveCard, addCard, findCard, deleteCard }) =>
           }
         : {
             isDragging: monitor.isDragging()
-          }
+          };
     },
     end: (item, monitor) => {
-      const itemDropped = monitor.didDrop()
+      const itemDropped = monitor.didDrop();
       if (item && item.isStillBeingDragged) {
         // deleteCard()
       } else if (item && !itemDropped) {
-        moveCard(item.id, item.originalIndex)
+        moveCard(item.id, item.originalIndex);
       }
     }
-  })
+  });
 
   const [, drop] = useDrop({
-    accept: 'card',
+    accept: "card",
     canDrop: () => false,
     hover: cardBeingDragged => {
       if (cardBeingDragged.cardFromSearch) {
         if (!findCard(cardBeingDragged.id)) {
-          const { index: overIndex } = findCard(id)
-          cardBeingDragged.originalIndex = overIndex
-          cardBeingDragged.cardFromSearch = false
-          cardBeingDragged.isStillBeingDragged = true
+          const { index: overIndex } = findCard(id);
+          cardBeingDragged.originalIndex = overIndex;
+          cardBeingDragged.cardFromSearch = false;
+          cardBeingDragged.isStillBeingDragged = true;
 
-          addCard(overIndex, cardBeingDragged)
+          addCard(overIndex, cardBeingDragged);
+          // addFakeCard(overIndex, { id: "-1", isFake: true });
         }
       } else {
         if (cardBeingDragged.id !== id) {
-          const { index: overIndex } = findCard(id)
-          moveCard(cardBeingDragged.id, overIndex)
+          const { index: overIndex } = findCard(id);
+          moveCard(cardBeingDragged.id, overIndex);
         }
       }
     }
-  })
+  });
 
   return (
     <div
@@ -58,18 +70,18 @@ const SortableCard = ({ id, label, moveCard, addCard, findCard, deleteCard }) =>
       id={id}
       label={label}
       style={{
-        border: 'solid 1px black',
-        margin: '20px',
-        height: '20px',
-        width: '100px',
-        opacity: isDragging ? '0' : '1'
+        border: "solid 1px black",
+        margin: "20px",
+        height: "20px",
+        width: "100px",
+        opacity: isDragging ? "0" : "1"
       }}
     >
       {label}
     </div>
-  )
-}
+  );
+};
 
-SortableCard.whyDidYouRender = false
+SortableCard.whyDidYouRender = false;
 
-export default SortableCard
+export default SortableCard;
